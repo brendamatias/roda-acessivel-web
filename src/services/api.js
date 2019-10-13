568de671e90394775814cc688ef3cad842922c2a
Brenda Matias <brendamatias.sobral@gmail.com>
2019-10-12 22:56:47 -0300

    fix: baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
+const url =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3333'
+    : 'http://159.89.186.143';
+
 const api = axios.create({
-  baseURL:
-    process.env.NODE_ENV === 'development'
-      ? 'http://localhost:3333'
-      : 'http://159.89.186.143',
+  baseURL: url,
 });
 
 export default api;
