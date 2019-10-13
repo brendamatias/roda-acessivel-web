8328fdae925c8fd9f0b101a8275de68cb89efd6b
Brenda Matias <brendamatias.sobral@gmail.com>
2019-10-12 22:46:58 -0300

    fix: baseURL corrected

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3333'
+      : 'http://159.89.186.143',
 });
 
 export default api;
