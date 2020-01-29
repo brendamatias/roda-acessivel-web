7bdf23eef14418aa8c2bc2ddb77d01e7297a2dcf
Brenda Matias <brendamatias.sobral@gmail.com>
2020-01-28 21:38:40 -0300

    chore: update README.md and api port

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,7 @@
 import axios from 'axios';
 
-const url =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3333'
-    : 'http://159.89.186.143';
-
 const api = axios.create({
-  baseURL: 'http://159.89.186.143',
+  baseURL: 'http://localhost:3333',
 });
 
 export default api;
